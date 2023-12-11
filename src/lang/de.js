b66767c96e4730d444e91c77a984fbdb751f3a54
Henry Voigt <oigty69@freenet.de>
2023-12-11 15:09:52 +0100

    added initial language dictionary

---
+++ src/lang/de.js
@@ -0,0 +1,13 @@
+export default function () {
+    return {
+        useCommaForNumberInUi: true,
+        dictionary: {
+            singIn: "Anmelden",
+            username: "Benutzername",
+            password: "Passwort",
+            menu: "Menü",
+            edit: "Editieren",
+            save: "Speichern",
+        }
+    };
+};
---
+++ src/lang/en.js
@@ -0,0 +1,13 @@
+export default function () {
+    return {
+        useCommaForNumberInUi: false,
+        dictionary: {
+            singIn: "Sign in",
+            username: "Username",
+            password: "Password",
+            menu: "Menu",
+            edit: "Edit",
+            save: "Save",
+        }
+    };
+};
