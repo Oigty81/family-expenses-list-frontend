1f0c4b78aa3845b6851b86b16a840ecaf63c5fa5
Henry Voigt <oigty69@freenet.de>
2024-06-22 00:31:15 +0200

    added language selector component

---
+++ src/components/ui/LanguageSelector.vue
@@ -0,0 +1,26 @@
+<script setup>
+  import {useAppStateStore} from "@stores/appState.js";
+
+  const appState = useAppStateStore();
+
+</script>
+
+<template>
+  <q-btn-toggle
+    v-model="appState.currentLanguageId"
+    toggle-color="green"
+    class="toggle-1 bg-white"
+    no-caps
+    unelevated
+    color="gray-10"
+    text-color="blue-7"
+    :options="[
+      {label: 'En', value: 1},
+      {label: 'De', value: 2},
+    ]"
+  />
+</template>
+
+<style scoped>
+
+</style>
