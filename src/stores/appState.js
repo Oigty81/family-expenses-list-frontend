4dad11118808e5302fa28f58fea830a2b6e25a1c
Henry Voigt <oigty69@freenet.de>
2024-07-03 00:37:16 +0200

    added tooltip delay to app state

--- src/stores/appState.js
+++ src/stores/appState.js
@@ -4,7 +4,7 @@ import { ref } from 'vue';
 export const useAppStateStore = defineStore('appStateStore', () => {
 
     const currentLanguageId = ref(1);
- 
+    const tooltipDelay = ref(800);
     const inputRequestDebounceTime = ref(300);
     const loadExpensesDebounceTime = ref(100);
     const intervalExpensesViewRefresh = ref(4000);
@@ -12,7 +12,7 @@ export const useAppStateStore = defineStore('appStateStore', () => {
     const isExpensesPageFiltersSetInitial = ref(true);
     
     return {
-        currentLanguageId,
+        currentLanguageId, tooltipDelay,
 		inputRequestDebounceTime, loadExpensesDebounceTime, intervalExpensesViewRefresh,
 		expensesPageFilters, isExpensesPageFiltersSetInitial
 		
